c99d13088d58e6fcb05d1feb5bd53b1124bd4add
TIBkey <81963789+baokeyit1177key@users.noreply.github.com>
2025-05-21 03:27:29 +0700

    setup
    setup

---
+++ src/test/java/org/example/roomiehub/RoomieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.roomiehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
