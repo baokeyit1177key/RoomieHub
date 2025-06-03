d13db22a4311325cf97e77c60324d37783e5bff9
vun30 <hoanvu3012@gmail.com>
2025-06-03 12:04:38 +0700

    fix accept token

--- src/main/java/org/example/roomiehub/config/JwtAuthenticationFilter.java
+++ src/main/java/org/example/roomiehub/config/JwtAuthenticationFilter.java
@@ -114,7 +114,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOriginPatterns("https://localhost:8080", "https://roomiehub-production.up.railway.app")
+                            .allowedOriginPatterns("http://localhost:8080", "https://roomiehub-production.up.railway.app")
 
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
