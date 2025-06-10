19f5d35ce3aace8499af529b851ed59d082ac27f
TIBkey <81963789+baokeyit1177key@users.noreply.github.com>
2025-06-11 01:34:22 +0700

    new config to detect crash ralway

---
+++ src/main/java/org/example/roomiehub/Enum/PersonalityTrait.java
@@ -0,0 +1,16 @@
+package org.example.roomiehub.Enum;
+
+ public enum PersonalityTrait {
+        QUIET,
+        OUTGOING,
+        NEAT,
+        RELAXED,
+        STUDIOUS,
+        CREATIVE,
+        EARLY_BIRD,
+        NIGHT_OWL,
+        FLEXIBLE,
+        SOCIABLE,
+        INTROVERTED,
+        EXTROVERTED
+    }
---
+++ src/main/java/org/example/roomiehub/service/RoommatePostService.java
@@ -0,0 +1,14 @@
+//package org.example.roomiehub.service;
+//
+//import org.example.roomiehub.dto.request.RoommatePostRequest;
+//import org.example.roomiehub.dto.response.RoommatePostResponse;
+//
+//import java.util.List;
+//
+//public interface RoommatePostService {
+//    RoommatePostResponse createRoommatePost(RoommatePostRequest request, String userEmail);
+//    RoommatePostResponse getRoommatePostById(Long id);
+//    List<RoommatePostResponse> getAllRoommatePosts();
+//    RoommatePostResponse updateRoommatePost(Long id, RoommatePostRequest request, String userEmail);
+//    void deleteRoommatePost(Long id, String userEmail);
+//}
