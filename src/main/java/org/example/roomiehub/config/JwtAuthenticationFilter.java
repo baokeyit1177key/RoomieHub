d72bb072e4b043bd02fe9df268fda15ac947a5e7
vun30 <hoanvu3012@gmail.com>
2025-06-13 17:23:24 +0700

    add cors localhost:5173

--- src/main/java/org/example/roomiehub/config/JwtAuthenticationFilter.java
+++ src/main/java/org/example/roomiehub/config/JwtAuthenticationFilter.java
@@ -100,7 +100,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                             ))
                     .servers(List.of(
                             new Server().url("https://roomiehub-production.up.railway.app"),
-                            new Server().url("http://localhost:8080")
+                            new Server().url("http://localhost:8080"),
+                            new Server().url("http://localhost:5173")
                     ));
         }
     }
@@ -114,7 +115,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOriginPatterns("http://localhost:8080", "https://roomiehub-production.up.railway.app")
+                            .allowedOriginPatterns("http://localhost:8080", "https://roomiehub-production.up.railway.app" , "http://localhost:5173")
 
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
