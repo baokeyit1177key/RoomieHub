7db1d0455ff2fe5a6ceaf67b2bbcf84b7e526f48
vun30 <hoanvu3012@gmail.com>
2025-07-26 00:17:10 +0700

    watch user posting

--- src/main/java/org/example/roomiehub/repository/UserPackageRepository.java
+++ src/main/java/org/example/roomiehub/repository/UserPackageRepository.java
@@ -3,9 +3,10 @@ package org.example.roomiehub.repository;
 import org.example.roomiehub.model.Payment;
 import org.example.roomiehub.model.UserPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserPackageRepository extends JpaRepository<UserPackage, Long> {
     Optional<UserPackage> findFirstByUserIdAndActiveTrueOrderByEndDateDesc(Long userId);
 
--- src/main/java/org/example/roomiehub/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/roomiehub/service/impl/UserServiceImpl.java
@@ -1,19 +1,23 @@
 package org.example.roomiehub.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.roomiehub.dto.response.UserPackageInfoResponse;
 import org.example.roomiehub.exception.NoActivePackageException;
 import org.example.roomiehub.model.UserPackage;
 import org.example.roomiehub.repository.UserPackageRepository;
 import org.example.roomiehub.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private  UserPackageRepository userPackageRepository;
+    private final UserPackageRepository userPackageRepository;
 
     @Override
     public UserPackageInfoResponse getCurrentUserPackageInfo(Long userId) {
         UserPackage activePackage = userPackageRepository
                 .findFirstByUserIdAndActiveTrueOrderByEndDateDesc(userId)
-                .orElseThrow(() -> new NoActivePackageException("Bạn chưa có gói đăng bài hợp lệ"));
+                .orElse(null);
 
         return new UserPackageInfoResponse(
                 activePackage.getPackageType(),
